b0d4d47ab69fc5ad9275bdec2735825e4744148b
Xiaohe Dong <dannyrivclo@gmail.com>
2021-04-16 11:37:48 -0700

    initial commit

---
+++ app/src/main/java/com/example/testpushnotificationandroid/Drawable.java
@@ -0,0 +1,11 @@
+package com.example.testpushnotificationandroid;
+
+public final class Drawable {
+    public static final int background=0x7f020000;
+    public static final int buttons=0x7f020001;
+    public static final int dead=0x7f020002;
+    public static final int enemy=0x7f020003;
+    public static final int ic_launcher=0x7f020004;
+    public static final int play_btn=0x7f020005;
+    public static final int player=0x7f020006;
+}
---
+++ app/src/main/java/com/example/testpushnotificationandroid/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.example.testpushnotificationandroid;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
